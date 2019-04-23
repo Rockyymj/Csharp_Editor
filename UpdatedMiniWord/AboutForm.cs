ee66b279f119fdca54fbd941fea71c748340011b
Rockyymj <37495081+Rockyymj@users.noreply.github.com>
2019-04-23 11:07:13 +0800

    Add files via upload

---
+++ UpdatedMiniWord/AboutForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdatedMiniWord
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
